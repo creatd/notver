5a2bc8fe5de335bc3ce68a9bd8a98791b825338d
d.onur.yavuz <d.onur.yavuz@d6b243bc-2c87-11df-bc59-2ffd2b2536e4>
2011-04-13 09:57:32 +0000

    arama kodu degisikligi, ders kodunu stringten boslukla ayiriyorum

--- trunk/notver/notver4/UserControls/AraDers.ascx.cs
+++ trunk/notver/notver4/UserControls/AraDers.ascx.cs
@@ -19,22 +19,16 @@ public partial class UserControls_AraDers : BaseUserControl
         try
         {
             string searchParams = dersIsmi.Text.ToString().Trim();
+            string aramaKriterleri = dersKoduAyir(searchParams);
 
-            if (string.IsNullOrEmpty(searchParams))
-            {
-                return;
-            }
-            else if (searchParams.StartsWith("Ismini"))
-            {
-                return;
-            }
             //Strip whitespaces and replace them with +
-            string[] words = searchParams.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] words = aramaKriterleri.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             StringBuilder sb = new StringBuilder();
             foreach (string word in words)
             {
                 sb.Append(word + "+");
             }
+
             //Sonda gereksiz bir + kaldi ama onemli degil
             Response.Redirect(Page.ResolveUrl("~/SearchResults.aspx") + "?SearchType=2&SearchParams=" + sb.ToString());
         }
@@ -42,5 +36,70 @@ public partial class UserControls_AraDers : BaseUserControl
         {
             Mesajlar.AdmineHataMesajiGonder(Request.Url.ToString(), ex.Message, session.KullaniciID, Enums.SistemHataSeviyesi.Orta);
         }
+    }
+
+    //gelen string icinde ders kodunu arar
+    protected string dersKodu(string searchParams)
+    {
+        int counter = 0;
+        int ascii;
+        string dersKodu = "";
+
+        CharEnumerator charEnum = searchParams.GetEnumerator();
+
+        while (charEnum.MoveNext())
+        {
+            ascii = Convert.ToInt32(searchParams[counter]);
+
+            if ((ascii >= 48) && (ascii <= 57))
+            {
+                //ders kodunun 3 haneli oldugu varsayimi var burda
+                //arama parametreleri arasinda ilk rakami gordugunde sonraki iki karakterle beraber
+                //return eder
+                dersKodu += Convert.ToString(searchParams[counter]);
+                dersKodu += Convert.ToString(searchParams[counter + 1]);
+                dersKodu += Convert.ToString(searchParams[counter + 2]);
+                break;
+            }
+
+            counter++;
+        }
+
+        return dersKodu;
+    }
+
+    protected string dersKoduAyir(string searchParams)
+    {
+        int ascii;
+        int counter = 0;
+        int length = searchParams.Length;
+        string dersKodu = "";
+        string dersIsmi = "";
+        string sonuc = searchParams;
+
+        CharEnumerator charEnum = searchParams.GetEnumerator();
+
+        while (charEnum.MoveNext())
+        {
+            ascii = Convert.ToInt32(searchParams[counter]);
+
+            if ((ascii >= 48) && (ascii <= 57))
+            {
+                for (int i = 0; i < length - counter; i++)
+                {
+                    dersKodu += Convert.ToString(searchParams[counter + i]);
+                }
+                break;
+            }
+            else
+            {
+                dersIsmi += Convert.ToString(searchParams[counter]);
+            }
+            counter++;
+        }
+
+        sonuc = dersIsmi + " " + dersKodu;
+
+        return sonuc;
     }
 }
